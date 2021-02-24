4f1872616c8e5bee44e47052d28609f412f69600
Abraham Ramos M <abraham.ramos.morales@uabc.edu.mx>
2021-02-23 19:20:21 -0800

    Variables y tipos de datos

--- practica-2/Program.cs
+++ practica-2/Program.cs
@@ -11,8 +11,19 @@ namespace practica_2
         static void Main(string[] args)
         {
             // Mostrar Hello World en la consola
-            System.Console.Write("Hello World");
-            Console.ReadLine();
+                // System.Console.Write("Hello World");
+            // Variables
+            var numero = 4;
+            var saludo = "hola";
+            System.Int32 wholeNumber = 4;
+            System.Double pi = 3.1416;
+            System.Boolean status = true;
+            System.String cadena = "hello";
+            System.Char letter = 'z';
+            System.Console.WriteLine(numero);
+            System.Console.WriteLine(saludo);
+            System.Console.WriteLine(numero + 2);
+            Console.ReadKey();
         }
     }
 }
