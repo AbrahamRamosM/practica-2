5d9aef50abe2b57776451ca88b9ad2ebd3a9673f
Abraham <abraham.ramos.morales@uabc.edu.mx>
2021-03-02 19:23:12 -0800

    Creacion de ejercicio con String y Int

---
+++ Ejercicio_StringInt/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_StringInt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declarar variables
+
+            String nombre;
+            int edad;
+
+            // Pedir datos
+
+            Console.WriteLine("Favor de escribir tu nombre:");
+            nombre = Console.ReadLine();
+            Console.WriteLine("\nFavor de escribir tu edad:");
+            string años = Console.ReadLine();
+            
+            // Mostrar datos
+
+            Console.Clear();
+            
+            if (int.TryParse(años, out edad)) {
+                Console.WriteLine("\nTe llamas " + nombre + " y tienes " + edad + " años.");
+            } else {
+                Console.WriteLine("\nTe llamas " + nombre + " y tienes " + años + " años.");
+            }
+
+            System.Console.ReadKey();
+        }
+    }
+}
