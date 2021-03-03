3269d524bffbde446a5338ab6e84db94669a7073
Abraham <abraham.ramos.morales@uabc.edu.mx>
2021-03-02 19:12:15 -0800

    Ejercicio con String

---
+++ Ejercicio_String/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // #1. Pedir por consola el nombre de una persona
+
+            String nombre;
+            String ciudad;
+
+            // #2. Pedir el nombre de una ciudad
+
+            Console.WriteLine("Favor de escribir tu nombre");
+            nombre = Console.ReadLine();
+
+            Console.WriteLine("\nEscribe tu ciudad");
+            ciudad = Console.ReadLine();
+
+            // #3. Mostrar nombre de ciudad en pantalla
+            System.Console.Clear();
+            System.Console.WriteLine("\nHola " + nombre + ", bienvenido a " + ciudad + ".");
+            System.Console.ReadKey();
+        }
+    }
+}
